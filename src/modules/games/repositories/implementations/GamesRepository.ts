1b1d6d83a5f1c5908426d2c3c87e44a2aa33f136
Felipe Pereira <felipepereiracg@gmail.com>
2021-03-27 21:36:45 -0300

    implementação desafios

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,22 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    return await this.repository
+      .createQueryBuilder("games")
+      .where("LOWER(games.title) LIKE LOWER(:title)",{title:`%${param}%`})
+      .getMany()
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT COUNT(id) FROM games"); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
+
+    const game = await this.repository.manager.findOne(Game, id);
+
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder().relation(Game,"users").of(game).loadMany();
+      
   }
 }
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,26 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    return await this.repository.findOneOrFail({id:user_id}, {relations:['games']});
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY first_name"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+
+    const fistFormated = first_name.toLocaleLowerCase();
+    const lastFormated = last_name.toLocaleLowerCase();
+
+    return await this.
+    repository.
+    query(
+      `SELECT first_name, last_name, email 
+    FROM users WHERE lower(first_name) = lower('${fistFormated}') and lower(last_name) = lower('${lastFormated}')`
+    ); // Complete usando raw query
   }
 }
